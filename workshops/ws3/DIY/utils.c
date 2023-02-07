2e397741611bd125ab5c71f67004a402f1c35698
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-02-07 16:58:18 -0500

    fardad's changes

--- workshops/ws3/DIY/utils.c
+++ workshops/ws3/DIY/utils.c
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+void flushKey(void) {
+	char ch = 'x';
+	while(ch != '\n') {
+		ch = getchar();
+	}
+}
 void line(char fill, int length) {
 	int i;
 
@@ -14,25 +19,26 @@ int getInt(void) {
 
 	int value;
 	int done = 0;
-
+	char ch;
 	while (!done) {
 		
-		if (scanf("%d", &value) == 1) {
+		if (scanf("%d", &value) == 1) {   // \n
+			ch = getchar();
+			// if ch is not new line,
+			//       print error (only an int)
+			//		   flush keyboard
+		   // else:
 			done = 1;
-			return value;
 		}
 		else if (scanf("%d", &value) != 1){
-			char ch = 'x';
-			while (ch != '\n') {
-				ch = getchar();
-			}
+			flushKey();
 			printf("Invalid Integer, try again: ");
 		}
 	}
+	return value;
 
 	/*it still prints user input integer even when other characters r inputed.ex: user input : 10abc, program output : you entered 10.
 	* also doesnt allow user to input integer from 10 to 20 when another character is inputed*/
-
 }
 
 int getIntMM(int min, int max) {
@@ -40,16 +46,13 @@ int getIntMM(int min, int max) {
 	int done = 0;
 
 	while (!done) {
-		if (min < scanf("%d", &value) < max) {
+		value = getInt();
+		if (min <= value && max >= value) {
 			done = 1;
-			return value;
 		}
-		else if (scanf("%d", &value) < min && scanf("%d", &value) > max) {
-			char ch = 'x';
-			while (ch != '\n') {
-				ch = getchar();
-			}
-			printf("Invalid Integer, try again: ");
+		else {
+			printf("Invalid Integer, try again: "); // fix the error message....
 		}
 	}
+	return value;
 }
