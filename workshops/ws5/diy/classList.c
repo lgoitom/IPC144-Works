32940d04f44bd12e80efae54c4354026fa4a5e60
Luwam Goitom-Worre <lgoitom-worre@myseneca.ca>
2023-02-22 22:54:28 -0400

    ws5 p2 updated again

--- workshops/ws5/diy/classList.c
+++ workshops/ws5/diy/classList.c
@@ -14,9 +14,9 @@ void subjectMarksReport(void) {
 
 	printf("Enter subject Name: ");
 	scanf("%s", subjectName);
-	printf("Enter the number of students (max 50): ");
+	printf("Enter Number of students (max 50): ");
 	scanf("%d", &numStudents);
-	printf("Enter %d student marks:\n", numStudents);
+	printf("Please enter %d student marks:\n", numStudents);
 	for (rowNum = 0; rowNum < numStudents; rowNum++) {
 
 		printf("%d of %d:\n", rowNum + 1, numStudents);
@@ -39,22 +39,22 @@ void printReport(const char subjectCode[], const int studentNumbers[],
 	int i;
 	int totalMarks = 0;
 
-	printf("  SUBJECT MARKS REPORT!\n");
-	printf("  +-------------------+\n");
-	printf("  | %-18s|\n", subjectCode);
-	printf("  +---+--------+------+\n");
+	printf(" SUBJECT MARKS REPORT!\n");
+	printf(" +-------------------+\n");
+	printf(" | %-18s|\n", subjectCode);
+	printf(" +---+--------+------+\n");
 	char stdNo[10] = "Std No";
 	char markStr[10] = "mark";
 	char row[10] = "Row";
-	printf("  |%s", row);
+	printf(" |%s", row);
 	printf("|%7s |", stdNo);
 	printf("%5s |\n", markStr);
-	printf("  +---+--------+------+\n");
+	printf(" +---+--------+------+\n");
 
 	for (i = 0; i < noOfStudents; i++) {
-		printf("  | %d | 0%-4d |%5d |\n", i + 1, studentNumbers[i], marks[i]);
+		printf(" | %d | 0%-4d |%5d |\n", i + 1, studentNumbers[i], marks[i]);
 	}
-	printf("  +---+--------+------+\n");
+	printf(" +---+--------+------+\n");
 	for (i = 0; i < noOfStudents; i++) {
 		totalMarks += marks[i];
 		average = totalMarks / noOfStudents;
@@ -70,11 +70,11 @@ void printReport(const char subjectCode[], const int studentNumbers[],
 	char avg[10] = "Average";
 	char max[10] = "Highest";
 	char min[10] = "Lowest";
-	printf("  | %-11s|", avg);
+	printf(" | %-11s|", avg);
 	printf("%5d |\n", average);
-	printf("  | %-11s|", max);
+	printf(" | %-11s|", max);
 	printf("%5d |\n", highest);
-	printf("  | %-11s|", min);
+	printf(" | %-11s|", min);
 	printf("%5d |\n", lowest);
-	printf("  +---+--------+------+\n");
+	printf(" +------------+------+\n");
 }
